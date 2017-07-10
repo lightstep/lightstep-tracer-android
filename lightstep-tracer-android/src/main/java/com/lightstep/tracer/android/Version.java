21170420251053b96ffa60576b474f446dbd7ae2
Sarah Haskins <sarahhaskins@gmail.com>
2017-07-10 09:35:39 -0700

    File missing from previous commit

--- lightstep-tracer-android/src/main/java/com/lightstep/tracer/android/Version.java
+++ lightstep-tracer-android/src/main/java/com/lightstep/tracer/android/Version.java
@@ -1,5 +1,5 @@
 package com.lightstep.tracer.android;
 
 class Version {
-    static final String LIGHTSTEP_TRACER_VERSION = "0.12.6";
+    static final String LIGHTSTEP_TRACER_VERSION = "0.12.7";
 }
