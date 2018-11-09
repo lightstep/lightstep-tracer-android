831e02369dcbc2d933efe27f4868d7f1a339a3c1
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-11-09 16:49:08 +0100

    VERSION 0.14.7

--- lightstep-tracer-android/src/main/java/com/lightstep/tracer/android/Version.java
+++ lightstep-tracer-android/src/main/java/com/lightstep/tracer/android/Version.java
@@ -1,5 +1,5 @@
 package com.lightstep.tracer.android;
 
 class Version {
-    static final String LIGHTSTEP_TRACER_VERSION = "0.14.6";
+    static final String LIGHTSTEP_TRACER_VERSION = "0.14.7";
 }
