86e84839ce96e1854e9e20bbfc0d5f0070ad08ad
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2020-12-01 20:35:09 +0100

    VERSION 0.30.2 (#52)

--- lightstep-tracer-android/src/main/java/com/lightstep/tracer/android/Version.java
+++ lightstep-tracer-android/src/main/java/com/lightstep/tracer/android/Version.java
@@ -1,5 +1,5 @@
 package com.lightstep.tracer.android;
 
 class Version {
-    static final String LIGHTSTEP_TRACER_VERSION = "0.30.1";
+    static final String LIGHTSTEP_TRACER_VERSION = "0.30.2";
 }
